a38fade48452147f9a1ecd32857304c3e9cab055
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-03-19 15:01:58 +0000

    Second webpage JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Brandon Brandhuber All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brandon Brandhuber
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+
+function doMathOnClick() {
+    // this function does basic math in javascript
+    document.getElementById('add-math').innerHTML = 'The Area is = ' + (8 + 9)
+    document.getElementById('multiply-math').innerHTML = 'The Perimeter is = ' + (8 * 9)
+}
